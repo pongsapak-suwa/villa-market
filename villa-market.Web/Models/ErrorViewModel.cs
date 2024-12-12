ce24b5074007707fcedefd06b46e41a8c6292be3
poiu <94011063+pongsapak-suwa@users.noreply.github.com>
2024-12-12 17:03:44 +0700

    Add project files.

---
+++ villa-market.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace villa_market.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
