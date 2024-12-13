b88e20963d54b6612833784c66c7b327d8bc91e7
poiu <94011063+pongsapak-suwa@users.noreply.github.com>
2024-12-13 15:53:29 +0700

    inite class and add docker compose

---
+++ villa-market.Domain/Entities/Villa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace villa_market.Domain.Entities
+{
+    internal class Villa
+    {
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        [Display(Name = "Price per night")]
+        [Range(10, 10000)]
+        public double Price { get; set; }
+        public int Sqft { get; set; }
+        [Range(1, 10)]
+        public int Occupancy { get; set; }
+        [NotMapped]
+        public string? ImageUrl { get; set; }
+        public DateTime? Created_Date { get; set; }
+        public DateTime? Updated_Date { get; set; }
+        [NotMapped]
+        public bool IsAvailable { get; set; } = true;
+    }
+}
