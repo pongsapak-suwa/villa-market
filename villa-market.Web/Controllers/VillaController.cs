24b3cbe8311f4da447d928794310b69207202c38
poiu <94011063+pongsapak-suwa@users.noreply.github.com>
2024-12-18 13:00:39 +0700

    Add/Custom Villa create

--- villa-market.Web/Controllers/VillaController.cs
+++ villa-market.Web/Controllers/VillaController.cs
@@ -1,19 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using villa_market.Domain.Entities;
 using villa_market.Infrastructure.Data;
 
 namespace villa_market.Web.Controllers
 {
+    [Authorize]
     public class VillaController : Controller
     {
-        private readonly ApplicationDbContext _db;
-        public VillaController(ApplicationDbContext db)
+        private readonly ApplicationDbContext _villaService;
+        public VillaController(ApplicationDbContext villaService)
         {
-            _db = db;
+            _villaService = villaService;
         }
+
         public IActionResult Index()
         {
-            var villas = _db.Villas.ToList();
+            var villas = _villaService.Villas.ToList();
             return View(villas);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("name", "The description cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+
+                _villaService.Add(obj);
+                _villaService.SaveChanges();
+                TempData["success"] = "The villa has been created successfully.";
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
     }
 }
